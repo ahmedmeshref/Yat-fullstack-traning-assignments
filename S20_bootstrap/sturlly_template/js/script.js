aa71e438e9394ab1a473e88ecec253244dc421ed
ahmedmeshref <a.meshref@alustudent.com>
2021-01-12 03:13:43 +0200

    finalize page 2

---
+++ S20_bootstrap/sturlly_template/js/script.js
@@ -0,0 +1,14 @@
+// window.onscroll = function() {scrollFunction()};
+
+// function scrollFunction() {
+//   if (document.body.scrollTop > 500 || document.documentElement.scrollTop > 500) {
+//     document.getElementById("navbar").classList.add("bg-white");
+//     document.getElementsByClassName("nav-link").forEach((ele) => {
+//       ele.style.color = "#000";
+//     });
+//     console.log(document.getElementsByClassName("nav-link"));
+
+//   } else {
+//     document.getElementById("navbar").classList.remove("bg-white");
+//   }
+// }
